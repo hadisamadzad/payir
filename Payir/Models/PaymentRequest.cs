d913a6115591afe2553b2b33bd7c4521a3496fc8
Pegah Khabaziyan <Pegah.Khabaziyan@gmail.com>
2020-02-15 16:20:05 +0330

    [PUSH]

--- Payir/Models/PaymentRequest.cs
+++ Payir/Models/PaymentRequest.cs
@@ -2,17 +2,17 @@
 {
     public class PaymentRequest
     {
-        public string ApiKey { get; private set; }
-        public int Amount { get; set; }
-        public string RedirectUrl { get; set; }
-        public string Mobile { get; set; }
+        public string api { get; private set; }
+        public int amount { get; set; }
+        public string redirect { get; set; }
+        public string mobile { get; set; }
         public string FactorNumber { get; set; }
         public string Description { get; set; }
         public string ValidCardNumber { get; set; }
 
         public void SetApiKey(string apiKey)
         {
-            ApiKey = apiKey;
+            api = apiKey;
         }
     }
 }
